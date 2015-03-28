4401909eb7b77a0169c693db9be0d0c200e8daea
zhenke <zhenke@zhenkedeMac-Pro.local>
2015-03-28 23:45:05 +0800

    change  to

---
+++ src/js/app-config.js
@@ -0,0 +1,23 @@
+// For any third party dependencies, like jQuery, place them in the lib folder.
+
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+    paths: {
+        backbone: '../vendor/backbone/backbone',
+        'font-awesome': '../vendor/font-awesome/fonts/*',
+        jquery: '../vendor/jquery/dist/jquery',
+        requirejs: '../vendor/requirejs/require',
+        underscore: '../vendor/underscore/underscore',
+        'backbone.localStorage': '../vendor/backbone.localStorage/backbone.localStorage',
+        qunit: '../vendor/qunit/qunit/qunit'
+    },
+    packages: [
+
+    ]
+});
+
+// Start loading the main app file. Put all of
+// your application logic in there.
+requirejs(['main']);
